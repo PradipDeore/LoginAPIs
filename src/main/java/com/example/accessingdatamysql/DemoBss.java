158b1ed7c6b155dc7b71cbb65028a836bd08e77e
bhushansinkar1988@gmail.com <bhushansinkar1988@gmail.com>
2021-07-19 16:48:47 +0530

    login4 added

--- src/main/java/com/example/accessingdatamysql/DemoBss.java
+++ src/main/java/com/example/accessingdatamysql/DemoBss.java
@@ -10,10 +10,19 @@ public class DemoBss {
     @PostMapping("/login2")
     public void login(@RequestParam() String username, @RequestParam String password) {
 
+
+
     }
     @PostMapping("/login3")
     public Response login3(@RequestParam() String username, @RequestParam String password) {
 return null;
+
+    }
+
+    @PostMapping("/login4")
+    public void login4(@RequestParam() String username, @RequestParam String password) {
+
+
 
     }
 }
