c4f7a261fcbdb53f027b720f569bdc7c77295cce
pradipvdeore@gmail.com <pradipvdeore@gmail.com>
2021-07-19 16:24:35 +0530

    login 2 api implemented

--- src/main/java/com/example/accessingdatamysql/DemoBss.java
+++ src/main/java/com/example/accessingdatamysql/DemoBss.java
@@ -1,4 +1,13 @@
 package com.example.accessingdatamysql;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+
 public class DemoBss {
+    @PostMapping("/login2")
+    public void login(@RequestParam() String username, @RequestParam String password) {
+
+    }
 }
